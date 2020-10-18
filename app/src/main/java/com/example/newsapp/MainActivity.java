d660273b725d197b1f7ec1f3b2cac24f464188ab
hp <chhabraashish123@gmail.com>
2020-10-18 14:47:29 +0530

    Adapter class updated and onclick listener added to Recyclerview

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements NewsItemClicked {
     RecyclerView mRecyclerView;
-
+    ArrayList<String> items=new ArrayList<String>(Arrays.asList("zee","aajtak","abp","abvp","timesnow","republic","zee",
+            "aajtak","abp","abvp","timesnow","republic","zee","aajtak","abp","abvp","timesnow","republic","zee"));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView=(RecyclerView)findViewById(R.id.list);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setHasFixedSize(true);
+        NewsListAdapter myadapter=new NewsListAdapter(items,this);
+        mRecyclerView.setAdapter(myadapter);
+    }
+
+    @Override
+    public void onItemClicked(String item) {
+        Toast.makeText(this, item+ " Clicked", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/newsapp/NewsListAdapter.java
+++ app/src/main/java/com/example/newsapp/NewsListAdapter.java
@@ -1,30 +1,55 @@
 package com.example.newsapp;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class NewsListAdapter extends RecyclerView.Adapter {
+public class NewsListAdapter extends RecyclerView.Adapter<NewsViewHolder> {
+
+    NewsItemClicked listen;
+    ArrayList<String> items;
+    public NewsListAdapter(ArrayList<String> lists,NewsItemClicked listener) {
+        this.items=lists;
+        this.listen=listener;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public NewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View itemView=inflater.inflate(R.layout.item_news,parent,false);
+        final NewsViewHolder viewHolder=new NewsViewHolder(itemView);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listen.onItemClicked(items.get(viewHolder.getAdapterPosition()));
+            }
+        });
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
+        String item=items.get(position);
+        holder.mTextView.setText(items.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 }
     class NewsViewHolder extends RecyclerView.ViewHolder{
+        TextView mTextView;
         public NewsViewHolder(@NonNull View itemView) {
             super(itemView);
+            this.mTextView=(TextView)itemView.findViewById(R.id.title);
      }
+}
+interface NewsItemClicked {
+    void onItemClicked(String item);
 }
